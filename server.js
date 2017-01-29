651d228e923f9aee91f30ee0d20ba8adde39ed53
damien <damienlopa.pro@gmail.com>
2017-01-29 15:24:00 +0100

    ook

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ io.on('connection', function (socket) {
     if (loggedUser !== undefined) {
       // Broadcast d'un 'service-message'
       var serviceMessage = {
-        text: 'User "' + loggedUser.username + '" disconnected',
+        text:  loggedUser.username + 'déconnecté',
         type: 'logout'
       };
       socket.broadcast.emit('service-message', serviceMessage);
@@ -153,11 +153,11 @@ io.on('connection', function (socket) {
       users.push(loggedUser);
       // Envoi et sauvegarde des messages de service
       var userServiceMessage = {
-        text: 'You logged in as "' + loggedUser.username + '"',
+        text: 'Connecté en tant que "' + loggedUser.username + '"',
         type: 'login'
       };
       var broadcastedServiceMessage = {
-        text: 'User "' + loggedUser.username + '" logged in',
+        text: 'User ' + loggedUser.username + ' est connecté',
         type: 'login'
       };
       socket.emit('service-message', userServiceMessage);
