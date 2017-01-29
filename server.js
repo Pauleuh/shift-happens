a5f879c2b374dc12bf4d8d44e8270f36dfef5e7f
damien <damienlopa.pro@gmail.com>
2017-01-29 15:28:07 +0100

    rrrr

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var expressHbs = require('express3-handlebars');
 var http = require('http');
-var server = app.listen(3000);
+var server = app.listen(process.env.PORT || 3000);
 var io = require('socket.io').listen(server);
 
 app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
